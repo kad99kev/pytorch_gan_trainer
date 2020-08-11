7742998bc3e499d58996fa71fd5f13ffa0aad480
Kevlyn Kadamala <kevlyn@gmail.com>
2020-08-11 16:07:28 +0530

    Saving images for dataset left

--- create_dataset.py
+++ create_dataset.py
@@ -5,7 +5,8 @@ import logging
 
 logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
 
-
+LIMIT = 5
+IMAGE_SIZE = 512
 DATASET_PATH = './dataset'
 
 # Make directory for dataset
@@ -33,12 +34,37 @@ logging.info("Loading Camera...")
 cap = cv2.VideoCapture(0)
 logging.info("Camera Loaded!")
 
+is_capturing = False
+images_captured = 0
+length = 0
+
 while True:
 
     ret, frame = cap.read()
 
+    frame = cv2.resize(frame, (IMAGE_SIZE, IMAGE_SIZE))
+
     cv2.imshow("Main Window", frame)
+
+    # Create new label
+    if cv2.waitKey(1) & 0xFF == ord('c'):
+        label = input('Enter label name: ')
+        label_path = DATASET_PATH + f'/{label}/'
+        if os.path.isdir(label_path):
+            os.mkdir(label_path)
+            is_capturing = True
+        else:
+            length = len([name for name in os.listdir(label_path) if os.path.isfile(name)])
+            print('Add to dataset')
+
+    if is_capturing:
+        image_path = label_path + f'{images_captured + length}.jpg'
+        cv2.imwrite(image_path, frame)
+        image_captured += 1
+        if image_captured == LIMIT:
+            is_capturing = False
     
+    # Quit
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
